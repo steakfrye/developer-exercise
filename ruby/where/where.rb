0d66b83eac1180c6b20b71189a69c5727f88c785
Dylan <dfryed@gmail.com>
2018-04-29 14:47:01 -0500

    Added support for partials in Where

--- ruby/where/where.rb
+++ ruby/where/where.rb
@@ -1,10 +1,24 @@
 module Where
 
   def where(search_params)
+
+    # iterate through given array of hashes
     for i in (0...self.length) do
-      @pos = self[i]
-      if !(@pos.select { |key,value| search_params[key] == value }).empty?
-        return [@pos]
+      # declare variables
+      @position = self[i]
+      @value = search_params.values[0]
+
+      # find match with Regexp as argument
+      if @value.class == Regexp
+        if (@position.select { |key,value| @value.match(key) })
+          return [@position]
+        end
+
+      else
+        # find exact match with one argument
+        if !(@position.select { |key,value| search_params[key] == value }).empty?
+          return [@position]
+        end
       end
     end
   end
