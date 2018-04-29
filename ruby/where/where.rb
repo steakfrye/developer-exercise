1debf1f83b4d0b377877fe0616882e1055676300
Dylan <dfryed@gmail.com>
2018-04-29 16:29:04 -0500

    Added support for multiple array elements.

--- ruby/where/where.rb
+++ ruby/where/where.rb
@@ -2,25 +2,29 @@ module Where
 
   def where(search_params)
 
+    @found = Array.new
+
     # iterate through given array of hashes
     for i in (0...self.length) do
       # declare variables
       @position = self[i]
       @value = search_params.values[0]
 
+
       # find match with Regexp as argument
       if @value.class == Regexp
         if (@position.select { |key,value| @value.match(key) })
-          return [@position]
+          @found.push(@position)
         end
 
       else
         # find exact match with one argument
         if !(@position.select { |key,value| search_params[key] == value }).empty?
-          return [@position]
+          @found.push(@position)
         end
       end
     end
+    return @found
   end
 
 end
