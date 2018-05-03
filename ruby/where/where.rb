d80ad1abbc246fe498da9f9db4a20567e0a7808a
Dylan <dfryed@gmail.com>
2018-05-02 20:39:35 -0500

    where module completed

--- ruby/where/where.rb
+++ ruby/where/where.rb
@@ -2,31 +2,44 @@ module Where
 
   def where(search_params)
 
-    @found = Array.new
+    # declare variables
+    found = Array.new
+    input = self
 
-    # iterate through given array of hashes
-    for i in (0...self.length) do
-      # declare variables
-      @position = self[i]
-      @value = search_params.values[0]
+    # iterate through each argument
+    for j in (0...search_params.length) do
+      key = search_params.keys[j]
+      value = search_params.values[j]
 
+      # if multiple arguments are given, make sure output matches all criteria
+      if j > 0
+        input = found
+        found = []
+      end
 
-      # find match with Regexp as argument
-      if @value.class == Regexp
-        if (@position.select { |key,value| @value.match(key) })
-          @found.push(@position)
-        end
+      # iterate through given array of hashes
+      for i in (0...input.length) do
+        position = input[i]
 
-      else
-        # find exact match with one argument
-        if !(@position.select { |key,value| search_params[key] == value }).empty?
-          @found.push(@position)
+        # find match with Regexp as argument
+        if value.class == Regexp
+          symbols = position.find_all { |key,value| search_params[key] }
+          # symbols variable returns array of arrays, therefore we need to visit 0 index (symbols[0].select)
+          if (symbols[0].select { |n| n.match(value) }).any?
+            found.push(position)
+          end
+
+        else
+          # find exact match
+          if not (position.select { |key,value| search_params[key] == value }).empty?
+            found.push(position)
+          end
         end
       end
     end
-    return @found
+    # return the found results!
+    return found
   end
-
 end
 
 class Array
